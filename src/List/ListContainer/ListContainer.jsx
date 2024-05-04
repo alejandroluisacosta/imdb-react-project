62fc756aa3cd8561bc11bbe2654a544fea2b2f10
Alejandro <alejandroluisacosta95@gmail.com>
2024-05-04 14:34:35 +0200

    add: styling improvements on movies list

--- src/List/ListContainer/ListContainer.jsx
+++ src/List/ListContainer/ListContainer.jsx
@@ -16,7 +16,7 @@ const ListContainer = () => {
     }
 
     return (
-        <>
+        <div class="InfoContainer">
             <h2>Tablas de IMDb</h2>
             <h1>Las 250 mejores películas de IMDb</h1>
             <p>Según la calificación de los votantes habituales de IMDb</p>
@@ -35,7 +35,7 @@ const ListContainer = () => {
                 <MoviesList display={display}/>
                 <MoreToExplore className="MoreToExplore"/>
             </div>
-        </>
+        </div>
     )
 }
 
--- src/List/MovieCard/MovieCard.jsx
+++ src/List/MovieCard/MovieCard.jsx
@@ -4,7 +4,7 @@ import './MovieCard.css'
 const MovieCard = (props) => {
 
     useEffect(() => {
-        const elements = document.querySelectorAll(`.${props.class} > *`);
+        const elements = document.querySelectorAll(`.${props.class} *`);
         elements.forEach(element => {
             const originalClass = element.className.split(' ')[0];
             if (props.class === 'MovieCard--grid') {
@@ -20,16 +20,9 @@ const MovieCard = (props) => {
         <div className={props.class}>
             <img className="MovieCard__image"src={props.image} />
             <div className="MovieCard__info">
-                <div>
-                    <p className="MovieCard__id">{props.id}.</p>
-                    <p className="MovieCard__title">{props.title}</p>
-                </div>
-                <p className="MovieCard__year">{props.year}</p>
-                <p className="MovieCard__duration">{props.duration}</p>
-                <div>
-                    <p className="MovieCard__rating">{props.rating}</p>
-                    <p className="MovieCard__voters">({props.voters})</p>
-                </div>
+                <p className="MovieCard__title">{props.id}. {props.title}</p>
+                <p className="MovieCard__yearAndDuration">{props.year}&nbsp;&nbsp;&nbsp;{props.duration}</p>
+                <p className="MovieCard__ratingAndVoters">{props.rating}&nbsp;&nbsp;&nbsp;({props.voters})</p>
             </div>
         </div>
     )
